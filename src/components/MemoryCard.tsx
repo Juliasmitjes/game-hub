15d329866713e25f4a909af6d69a05fd7aecf542
lilJ <julia.s@live.nl>
2025-10-15 16:14:53 +0200

    memorycard

--- src/components/MemoryCard.tsx
+++ src/components/MemoryCard.tsx
@@ -12,7 +12,7 @@ function MemoryCard({item, id, handleClick}: MemoryCardProps){
   return (
   <div className="h-full">   
     <div
-      className={`card ${item.stat} m-0 p-0 h-full bg-myOcean flex justify-center items-center rounded-md cursor-pointer shadow-lg border-[6px] border-myOcean`}
+      className={`card ${item.stat} m-0 p-0 h-full bg-accent flex justify-center items-center rounded-md cursor-pointer shadow-lg border-2 transition-all duration-300 hover:bg-primary/20  hover:scale-105`}
       onClick={() => handleClick(id)}
       >
       <img src={item.img} alt="" className="card-img max-w-7/10" />
